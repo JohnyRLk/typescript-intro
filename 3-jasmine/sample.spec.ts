bc50ddda65d9b56a2141ecda0ac0e7e92094cfe8
JohnyRLk <jan.rycielski@gmail.com>
2021-07-12 15:02:55 +0200

    improve sample.spec.ts from 3-jasmine: two it

--- 3-jasmine/sample.spec.ts
+++ 3-jasmine/sample.spec.ts
@@ -2,14 +2,17 @@ import "jasmine";
 import { GeneratePESEL } from "../1-typescript-intro/sample-5-modules-1";
 
 describe("Check if generator works coretly", () => {
-  it("Check length of pesel and check data is on right place", () => {
+  let b: Date = new Date("1979-05-21");
+  const generatePesel: string = GeneratePESEL(b);
+  console.log(generatePesel);
+  it("Check data is on right place", () => {
     let checkDate: string = "79521";
-    let b: Date = new Date("1979-05-21");
-    const generatePesel = GeneratePESEL(b);
-    console.log(generatePesel);
     let checkDateExists: boolean = checkDate === generatePesel.slice(0, 5);
+    expect(checkDateExists).toBe(true);
+  });
+  it("Check length of pesel", () => {
     let checkLength: boolean = generatePesel.length === 10;
-    expect(checkLength && checkDateExists).toBe(true);
+    expect(checkLength).toBe(true);
   });
 });
 // Zadanie: Napisz test dla stworzonego generatora
